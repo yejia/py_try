3e6e3a42b56706fc874d60af1e6474342a4640cd
yejia <yejia2000@gmail.com>
2018-09-02 19:48:58 +0800

    added scripts for better displaying of steps of program execution and with manual control

---
+++ pause.py
@@ -0,0 +1,54 @@
+import sys, termios, atexit
+from select import select
+
+# save the terminal settings
+fd = sys.stdin.fileno()
+new_term = termios.tcgetattr(fd)
+old_term = termios.tcgetattr(fd)
+
+# new terminal setting unbuffered
+new_term[3] = (new_term[3] & ~termios.ICANON & ~termios.ECHO)
+
+# switch to normal terminal
+def set_normal_term():
+    termios.tcsetattr(fd, termios.TCSAFLUSH, old_term)
+
+# switch to unbuffered terminal
+def set_curses_term():
+    termios.tcsetattr(fd, termios.TCSAFLUSH, new_term)
+
+def putch(ch):
+    sys.stdout.write(ch)
+
+def getch():
+    return sys.stdin.read(1)
+
+def getche():
+    ch = getch()
+    putch(ch)
+    return ch
+
+def kbhit():
+    dr,dw,de = select([sys.stdin], [], [], 0)
+    return dr <> []
+
+from time import sleep
+
+if __name__ == '__main__':
+    is_asleep = False
+    atexit.register(set_normal_term)
+    set_curses_term()
+
+    while 1:
+        if kbhit():            
+            ch = getch()
+            #* above won't be deteced when the program is in sleep, so below won't work
+            if is_asleep:
+                is_asleep = False
+                continue
+            else:        
+                is_asleep = True                
+                sleep(6)
+        sys.stdout.write('.')
+
+    print 'done'
